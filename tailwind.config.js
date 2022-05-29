5cb490cacf1e78ac00228804cce230c2e83c1ce1
andymartin-work <106444848+andymartin-work@users.noreply.github.com>
2022-05-28 22:42:55 -0700

    add tailwind
    add tailwind, remove extra navbar comments, fix py on headers

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+
+    extend: {
+      colors:{
+        myGreentail:'#00a797'
+  
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+}
